8c27929db6505cb3c7875330c3d74b7892b0d17f
Rhayza Pereira Fortuna <rhayza_fortuna@hotmail.com>
2023-05-16 13:32:07 -0300

    atualizando de novo

--- src/app/corpo/xp/xp.component.ts
+++ src/app/corpo/xp/xp.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-xp',
   templateUrl: './xp.component.html',
   styleUrls: ['./xp.component.scss']
+
 })
 export class XpComponent {
+  stepPosition: number = 1;
 
+  @Input() empresa: string = 'sesi';
+
+  teste(step:string) {
+    this.empresa = step
+    if (this.empresa === 'sesi') {
+
+      this.stepPosition = 1;
+    }
+    if (this.empresa === 'gazeta') {
+ 
+      this.stepPosition = 20;
+    }
+    if (this.empresa === 'tim') {
+
+      this.stepPosition = 40;
+    }
+    if (this.empresa === 'saraiva') {
+
+      this.stepPosition = 60;
+    }
+    if (this.empresa === 'prefeitura') {
+
+      this.stepPosition = 80;
+    }
+    if (this.empresa === 'atual') {
+
+      this.stepPosition = 100;
+    }
+  }
 }
